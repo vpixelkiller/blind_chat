2d8622cc54a8eb0fbe881b4bc8c96fdd6612a857
vdelrio-latency <vdelrio@latencydata.com>
2025-12-17 20:00:12 +0100

    Delete code for test

--- www/login-simple/session_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "session.cookie_httponly = " . ini_get('session.cookie_httponly') . "<br>";
-echo "session.cookie_secure = " . ini_get('session.cookie_secure') . "<br>";
-echo "session.use_strict_mode = " . ini_get('session.use_strict_mode') . "<br>";
-echo "session.cookie_samesite = " . ini_get('session.cookie_samesite') . "<br>";
-echo "session.gc_maxlifetime = " . ini_get('session.gc_maxlifetime') . "<br>";
-?>
-
